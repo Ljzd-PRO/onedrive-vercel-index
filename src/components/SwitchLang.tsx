d400709662289b8dbc07566d89d376fe34b578b3
ERZATZ Type AK9 <ljzd@office.ljzd-pro.ml>
2023-06-15 05:36:39 +0800

    Update SwitchLang.tsx

--- src/components/SwitchLang.tsx
+++ src/components/SwitchLang.tsx
@@ -32,7 +32,7 @@ const localeText = (locale: string): string => {
     case 'tr-TR':
       return '🇹🇷 Türkçe'
     case 'zh-TW':
-      return '🇹🇼 繁體中文'
+      return '繁體中文'
     default:
       return '🇬🇧 English'
   }
