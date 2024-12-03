ccdec32ef02eba7bb773abbd2f10d53a85072c84
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2024-12-03 16:02:08 +0800

    feat: optimize token storage with local caching

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -4,14 +4,21 @@ import siteConfig from '../../config/site.config'
 // Persistent key-value store is provided by Redis, hosted on Upstash
 // https://vercel.com/integrations/upstash
 const kv = new Redis(process.env.REDIS_URL || '')
+let localAccessToken: string | null
+let localRefreshToken: string | null
+let accessTokenExpiryAt: number
 
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-  const accessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
-  const refreshToken = await kv.get(`${siteConfig.kvPrefix}refresh_token`)
-
+  localRefreshToken = localRefreshToken || (await kv.get(`${siteConfig.kvPrefix}refresh_token`))
+  if (!localAccessToken || Date.now() < accessTokenExpiryAt) {
+    localAccessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
+    accessTokenExpiryAt = await kv
+      .ttl(`${siteConfig.kvPrefix}access_token`)
+      .then(ttl => (ttl >= 0 ? Date.now() + ttl * 1000 : 0))
+  }
   return {
-    accessToken,
-    refreshToken,
+    accessToken: localAccessToken,
+    refreshToken: localRefreshToken,
   }
 }
 
@@ -26,4 +33,7 @@ export async function storeOdAuthTokens({
 }): Promise<void> {
   await kv.set(`${siteConfig.kvPrefix}access_token`, accessToken, 'EX', accessTokenExpiry)
   await kv.set(`${siteConfig.kvPrefix}refresh_token`, refreshToken)
+  localAccessToken = accessToken
+  accessTokenExpiryAt = Date.now() + accessTokenExpiry * 1000
+  localRefreshToken = refreshToken
 }
